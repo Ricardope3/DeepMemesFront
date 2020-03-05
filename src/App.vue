1513525163d7863ce4e06bb337337b58ae835aa6
ricardope3 <ricky.foals@gmail.com>
2020-03-04 20:29:17 -0600

    hola

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <div class="bg-blue-500 flex-1 flex overflow-y-hidden">
         <div class="sidebar bg-gray-300 w-1/4 flex-none">Sidebar</div>
         <div class="content-area bg-gray-200 flex-1 overflow-y-auto">
-          <MemeCard :meme="meme"/>
+          <MemeCard :meme="meme" />
         </div>
         <div class="sidebar bg-gray-300 w-1/4 flex-none">Sidebar</div>
       </div>
@@ -20,11 +20,17 @@ export default {
   components: {
     MemeCard
   },
-  data : ()=>({
-    meme:{
-      profileUrl:"",
-      profileName:"Unusual Mexican Images",
-      
+  data: () => ({
+    meme: {
+      profileUrl:
+        "https://partycity6.scene7.com/is/image/PartyCity/_pdp_sq_?$_1000x1000_$&$product=PartyCity/279978_01",
+      profileName: "Unusual Mexican Images",
+      memeUrl:
+        "https://scontent.fntr4-1.fna.fbcdn.net/v/t1.0-9/88099451_839258536540784_962865817324617728_n.jpg?_nc_cat=1&_nc_sid=110474&_nc_oc=AQnXAhzOKVvHsP-dvHpFXDDnuEn2OXx8CY7KmsDOTs3g_4K1zVLyym1act60tXvcIWQ&_nc_ht=scontent.fntr4-1.fna&oh=3e9643d8e18f0a8cab55afb4fdfc9035&oe=5E983130",
+      title: "En los quince años",
+      likes: 2,
+      dislikes: 4,
+      comments: 13
     }
   })
 };
@@ -35,7 +41,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
--- src/components/MemeCard.vue
+++ src/components/MemeCard.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>
-      LMo
+  <div class="shadow-xl bg-white rounded-lg m-16 p-5">
+    <div class="topbar flex text">
+      <img :src="meme.profileUrl" class="rounded-full w-12 flex-initial" />
+      <div class="flex-auto pt-3 pl-3 text-lg">{{meme.profileName}}</div>
+    </div>
+    <div class="memeImage my-3">
+      <img :src="meme.memeUrl" class="rounded-lg shadow-xl" />
+    </div>
+    <div class="bottombar flex">
+      <img
+        src="https://cdn.pixabay.com/photo/2016/11/30/18/14/like-1873541_960_720.png"
+        class="w-12 rounded-full flex-initial"
+      />
+      <div style="width:15px"></div>
+      <img
+        src="https://cdn.pixabay.com/photo/2016/11/30/18/14/like-1873541_960_720.png"
+        class="w-12 rounded-full flex-initial" style="transform: rotate(180deg) scaleX(-1);"
+      />
+      <div style="width:390px"></div>
+      <img src="https://www.pngkey.com/png/detail/82-824259_chat-comments-chat-vector-icon-png.png" class="w-10 h-10 rounded-lg">
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: ["meme"]
+};
 </script>
 
 <style>
-
 </style>
