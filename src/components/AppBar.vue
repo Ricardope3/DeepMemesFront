fa61df538f3160cc49ad1f2a0e658d46a89bdc61
FranciscoMarquez1 <fcomarquezb@hotmail.com>
2020-03-04 20:07:20 -0600

    Add icons to AppBar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="w-screen flex flex-row bg-gray-800">
-    <div class="w-1/3">
+  <div class="w-screen flex flex-row bg-gray-800 h-14 p-2">
+    <div class="flex flex-row w-1/3">
+      <div class="w-1/3"/>
+      <div class="w-1/3"><img class="w-8 center-image" src="../assets/icons/email.svg"/>
+    </div>
+      <div class="w-1/3"><img class="w-8 center-image" src="../assets/icons/notification.svg"/>
+    </div>
     </div>
     <div class="w-1/3">
-        <h1 class="text-white text-center">Deep Memes</h1>
+        <h1 class="text-white text-center text-2xl">Deep Memes</h1>
     </div>
-    <div class="w-1/3">
+    <div class="flex flex-row w-1/3">
+      <div class="w-1/3"><img class="w-8 center-image" src="../assets/icons/search.svg"/>
+    </div>
+      <div class="w-1/3"><img class="w-8 center-image" src="../assets/icons/account.svg"/>
+    </div>
     </div>
   </div>
 </template>
@@ -19,5 +28,8 @@ export default {
 </script>
 
 <style>
-
+  .center-image {
+    margin-left: auto;
+    margin-right: auto;
+  }
 </style>
