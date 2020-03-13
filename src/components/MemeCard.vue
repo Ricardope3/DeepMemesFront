bc6536dd14a1be5d44f8894f7b4446e317cef860
ricardope3 <ricky.foals@gmail.com>
2020-03-12 20:29:57 -0600

    k

--- src/components/MemeCard.vue
+++ src/components/MemeCard.vue
@@ -1,25 +1,13 @@
 <template>
-  <div class="shadow-xl bg-white rounded-lg m-16 p-5">
-    <div class="topbar flex text">
-      <img :src="meme.profileUrl" class="rounded-full w-12 flex-initial" />
+  <div class="shadow-xl bg-white rounded-lg m-16 p-6">
+    <div class="title-bar flex pb-4">
+      <img :src="meme.profileUrl" class="rounded-full md:w-12 md:h-12 xsm:h-10 flex-initial xsm:w-10 mr-2" />
       <div class="flex-auto pt-3 pl-3 text-lg">{{meme.profileName}}</div>
     </div>
-    <div class="memeImage my-3">
+    <div class="memeImage">
       <img :src="meme.memeUrl" class="rounded-lg shadow-xl" />
     </div>
-    <div class="bottombar flex">
-      <img
-        src="https://cdn.pixabay.com/photo/2016/11/30/18/14/like-1873541_960_720.png"
-        class="w-12 rounded-full flex-initial"
-      />
-      <div style="width:15px"></div>
-      <img
-        src="https://cdn.pixabay.com/photo/2016/11/30/18/14/like-1873541_960_720.png"
-        class="w-12 rounded-full flex-initial" style="transform: rotate(180deg) scaleX(-1);"
-      />
-      <div style="width:390px"></div>
-      <img src="https://www.pngkey.com/png/detail/82-824259_chat-comments-chat-vector-icon-png.png" class="w-10 h-10 rounded-lg">
-    </div>
+    <div class="like-bar flex"></div>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   separator: ':',
   theme: {
     screens: {
+      xsm: '200px',
       sm: '640px',
       md: '768px',
       lg: '1024px',
