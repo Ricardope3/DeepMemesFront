9743b3ef79feabb0c6167bec42063cc747e166da
FranciscoMarquez1 <fcomarquezb@hotmail.com>
2020-06-10 15:14:43 -0500

    Corrected appbar and register

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -2,20 +2,17 @@
   <div class="w-screen flex flex-row bg-gray-800 h-14 p-2">
     <div class="flex flex-row w-1/3">
       <div class="w-1/3" />
-      <div class="w-1/3">
-        <img class="w-8 center-image" src="../assets/icons/email.svg" />
-      </div>
-      <div class="w-1/3">
-        <img class="w-8 center-image" src="../assets/icons/notification.svg" />
+      <div class="w-1/3"  @click="onLogOut">
+        <img class="w-8 center-image" src="../assets/icons/logout.svg" />
       </div>
     </div>
     <div class="w-1/3">
       <h1 class="text-white text-center text-2xl">Deep Memes</h1>
     </div>
     <div class="flex flex-row w-1/3">
-      <div class="w-1/3 profile" @click="onLogOut" >
-        <img class="w-8 center-image" src="../assets/icons/search.svg" />
-      </div>
+      <router-link to="/register" tag="div" router class="w-1/3 profile" >
+        <img class="w-8 center-image" src="../assets/icons/login.svg" />
+      </router-link>
       <router-link to="/profile" tag="div" router class="w-1/3 profile">
         <img class="w-8 center-image" src="../assets/icons/account.svg" />
       </router-link>
@@ -37,6 +34,7 @@ export default {
     onLogOut() {
       this.logout().then(() => {
         this.$router.replace({ name: "register" });
+        location.reload(true);
       });
     }
   }
