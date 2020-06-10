9d9cbb59468a3f072a0a0237332af13eb2092752
ricardope3 <ricky.foals@gmail.com>
2020-06-10 17:56:51 -0500

    headers

--- src/store/modules/memes.js
+++ src/store/modules/memes.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import user from './auth'
 const state = { memes: [], tags: localStorage.tags ? JSON.parse(localStorage.tags) : [] };
-const getters = { 
-    allMemes: () => state.memes, 
+const getters = {
+    allMemes: () => state.memes,
     getUserTags: () => state.tags
 };
 const actions = {
@@ -10,10 +10,11 @@ const actions = {
         let tagsCSV = ""
         if (state.tags.length === 0) {
             console.log("RANDOM")
-            const response = await axios.get(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/getRandom`,{
+            const response = await axios.get(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/getRandom`, {
                 headers: {
                     'Access-Control-Allow-Origin': '*',
                     'Access-Control-Allow-Methods': '*',
+                    ' Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept, Authorization"
                 }
             });
             commit('setMemes', response.data);
@@ -22,29 +23,32 @@ const actions = {
                 tagsCSV += `${state.tags[index]},`
             }
             console.log("TAGS")
-            const response = await axios.get(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/getRelatedTo?tags=${tagsCSV}`,{
+            const response = await axios.get(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/getRelatedTo?tags=${tagsCSV}`, {
                 headers: {
                     'Access-Control-Allow-Origin': '*',
                     'Access-Control-Allow-Methods': '*',
+                    ' Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept, Authorization"
                 }
             });
             commit('setMemes', response.data);
         }
     },
     async fetchRandomMemes({ commit }) {
-        const response = await axios.get("http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/getRandom",{
+        const response = await axios.get("http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/getRandom", {
             headers: {
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': '*',
+                ' Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept, Authorization"
             }
         });
         commit('setMemes', response.data);
     },
     async postMeme({ commit }) {
-        const response = await axios.post("http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/submission",{
+        const response = await axios.post("http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/submission", {
             headers: {
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': '*',
+                ' Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept, Authorization"
             }
         });
         commit('setMemes', response.data);
@@ -56,48 +60,53 @@ const actions = {
             headers: {
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': '*',
+                ' Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept, Authorization"
             }
         });
         user.state.user.link = body.profilePictureLink
         commit.lmaoIgnoraEsto
     },
     async createMeme({ commit }, body) {
-        await axios.post(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/createMeme`, body,{
+        await axios.post(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/createMeme`, body, {
             headers: {
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': '*',
+                ' Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept, Authorization"
             }
         });
         commit.lmaoIgnoraEsto
     },
     async getProfilePicture({ commit }, email) {
-        const response = await axios.get(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/getUser?email=${email}`,{
+        const response = await axios.get(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/getUser?email=${email}`, {
             headers: {
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': '*',
+                ' Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept, Authorization"
             }
         })
         commit.lmaoIgnoraEsto
         return response.data.profilePictureLink
     },
     async reaction({ commit }, body) {
-        await axios.post(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/reaction`, body,{
+        await axios.post(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/reaction`, body, {
             headers: {
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': '*',
+                ' Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept, Authorization"
             }
         })
         if (body.type === 0 && body.tags) {
 
-            localStorage.tags = localStorage.tags ? JSON.stringify([...JSON.parse(localStorage.tags),...body.tags]) : JSON.stringify(body.tags)
+            localStorage.tags = localStorage.tags ? JSON.stringify([...JSON.parse(localStorage.tags), ...body.tags]) : JSON.stringify(body.tags)
             commit("setTags", body.tags)
         }
     },
     async uploadComment({ commit }, body) {
-        await axios.post(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/createComment`, body,{
+        await axios.post(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/createComment`, body, {
             headers: {
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': '*',
+                ' Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept, Authorization"
             }
         })
         commit.lmaoIgnoraEsto
