d0e6cc31fa1784fc945c31feb0be765d8b6e57c4
ricardope3 <ricky.foals@gmail.com>
2020-06-10 17:50:05 -0500

    s

--- src/store/modules/memes.js
+++ src/store/modules/memes.js
@@ -10,23 +10,43 @@ const actions = {
         let tagsCSV = ""
         if (state.tags.length === 0) {
             console.log("RANDOM")
-            const response = await axios.get(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/getRandom`);
+            const response = await axios.get(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/getRandom`,{
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': '*',
+                }
+            });
             commit('setMemes', response.data);
         } else {
             for (let index in state.tags) {
                 tagsCSV += `${state.tags[index]},`
             }
             console.log("TAGS")
-            const response = await axios.get(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/getRelatedTo?tags=${tagsCSV}`);
+            const response = await axios.get(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/getRelatedTo?tags=${tagsCSV}`,{
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': '*',
+                }
+            });
             commit('setMemes', response.data);
         }
     },
     async fetchRandomMemes({ commit }) {
-        const response = await axios.get("http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/getRandom");
+        const response = await axios.get("http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/getRandom",{
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*',
+            }
+        });
         commit('setMemes', response.data);
     },
     async postMeme({ commit }) {
-        const response = await axios.post("http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/submission");
+        const response = await axios.post("http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/submission",{
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*',
+            }
+        });
         commit('setMemes', response.data);
     },
     // { commit },
@@ -42,16 +62,31 @@ const actions = {
         commit.lmaoIgnoraEsto
     },
     async createMeme({ commit }, body) {
-        await axios.post(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/createMeme`, body);
+        await axios.post(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/createMeme`, body,{
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*',
+            }
+        });
         commit.lmaoIgnoraEsto
     },
     async getProfilePicture({ commit }, email) {
-        const response = await axios.get(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/getUser?email=${email}`)
+        const response = await axios.get(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/getUser?email=${email}`,{
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*',
+            }
+        })
         commit.lmaoIgnoraEsto
         return response.data.profilePictureLink
     },
     async reaction({ commit }, body) {
-        await axios.post(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/reaction`, body)
+        await axios.post(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/reaction`, body,{
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*',
+            }
+        })
         if (body.type === 0 && body.tags) {
 
             localStorage.tags = localStorage.tags ? JSON.stringify([...JSON.parse(localStorage.tags),...body.tags]) : JSON.stringify(body.tags)
@@ -59,7 +94,12 @@ const actions = {
         }
     },
     async uploadComment({ commit }, body) {
-        await axios.post(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/createComment`, body)
+        await axios.post(`http://ec2-107-21-32-145.compute-1.amazonaws.com:8080/createComment`, body,{
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*',
+            }
+        })
         commit.lmaoIgnoraEsto
     },
 };
